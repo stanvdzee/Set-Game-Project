91aec9b9b628806fb529c0319d9381f83f847daf
DaRealNiekie <niekdehaan1003@gmail.com>
2025-12-09 13:52:31 +0100

    rules gemaakt ( kan verbetering krijgen)

--- rules.php
+++ rules.php
@@ -9,66 +9,78 @@
 
 
 <body>
-    Het doel van het spel is om een ‘Set’ van drie kaarten te identificeren uit 12 kaarten die op tafel liggen. Elke
-    kaart heeft een variatie van de volgende vier kenmerken:
-    <br>
-    <br>
-    KLEUR: Elke kaart is rood, groen of blauw.
-    SYMBOLEN: Elke kaart bevat ovalen, kronkels (squiggles) of ruiten.
-    AANTAL: Elke kaart heeft één, twee of drie symbolen.
-    ARCEERSTIJL: Elke kaart is gevuld (solid), open of gestreept (striped).
-    Een ‘Set’ bestaat uit drie kaarten waarbij elk kenmerk ofwel hetzelfde is op alle drie kaarten, ofwel verschillend
-    is op alle drie kaarten.
-    <br>
-    <br>
-    Met andere woorden: voor elk kenmerk in de ‘Set’ geldt dat het óf bij alle drie kaarten gelijk is, óf bij alle drie
-    verschillend.
-    <Br>
-    <br>
+    <div style="text-align: center; list-style-position: inside; ">
+
+        <p>
+            Het doel van het spel is om een ‘Set’ van drie kaarten te identificeren uit 12 kaarten die op tafel liggen.
+            Elke
+            kaart heeft een variatie van de volgende vier kenmerken:
+            <br>
+            <br>
+            KLEUR: Elke kaart is rood, groen of blauw.
+            SYMBOLEN: Elke kaart bevat ovalen, kronkels (squiggles) of ruiten.
+            AANTAL: Elke kaart heeft één, twee of drie symbolen.
+            ARCEERSTIJL: Elke kaart is gevuld (solid), open of gestreept (striped).
+            Een ‘Set’ bestaat uit drie kaarten waarbij elk kenmerk ofwel hetzelfde is op alle drie kaarten, ofwel
+            verschillend
+            is op alle drie kaarten.
+            <br>
+            <br>
+            Met andere woorden: voor elk kenmerk in de ‘Set’ geldt dat het óf bij alle drie kaarten gelijk is, óf bij
+            alle
+            drie
+            verschillend.
+            <Br>
+            <br>
 
 
-    Het deck van het spel bestaat uit 81 kaarten. Elke kaart bestaat maar 1 keer. Dus de kaart: ovaal gevuld rood 3 zit
-    maar 1 keer in het deck.
-    <br>
-    <br>
-    Voorbeelden van Sets
-    Voorbeeld set 1:
-    Kleur: verschillend op elke kaart
-    Symbool: hetzelfde op elke kaart (ovaal)
-    Aantal: hetzelfde op elke kaart (twee)
-    Arceerstijl: hetzelfde op elke kaart (gevuld)
-    ovaal gevuld blauw 2
-    ovaal gevuld rood 2
-    ovaal gevuld groen 2
-    <br>
-    <br>
-    Voorbeeld set 2:
-    Kleur: verschillend op elke kaart
-    Symbool: verschillend op elke kaart
-    Aantal: verschillend op elke kaart
-    Arceerstijl: verschillend op elke kaart
-    <br>
-    <br>
-    ruit gestreept blauw 2
-    kronkel gevuld rood 1
-    ovaal open groen 3
-    Kleur: hetzelfde op elke kaart (groen)
-    Voorbeeld set 3:
-    Kleur: hetzelfde op elke kaart (groen)
-    Symbool: hetzelfde op elke kaart (ruit)
-    Aantal: verschillend op elke kaart
-    Arceerstijl: verschillend op elke kaart
-    <br>
-    <br>
-    ruit gevuld groen 2
-    ruit gestreept groen 3
-    ruit open groen 1
-    <br>
-    <br>
-    <br>
-    <button class="button" onclick="window.location.href='set.test';">
-        ga terug naar de menu
-    </button>
+            Het deck van het spel bestaat uit 81 kaarten. Elke kaart bestaat maar 1 keer. Dus de kaart: ovaal gevuld
+            rood 3
+            zit
+            maar 1 keer in het deck.
+            <br>
+            <br>
+            Voorbeelden van Sets <br><br>
+            <b>Voorbeeld set 1:<br></b>
+
+            Kleur: verschillend op elke kaart<br>
+            Symbool: hetzelfde op elke kaart (ovaal)<br>
+            Aantal: hetzelfde op elke kaart (twee)<br>
+            Arceerstijl: hetzelfde op elke kaart (gevuld)<br>
+        <ul>
+            <li>ovaal gevuld blauw 2</li>
+            <li>ovaal gevuld rood 2</li>
+            <li>ovaal gevuld groen 2</li>
+        </ul>
+
+        <b> Voorbeeld set 2:<br></b>
+        Kleur: verschillend op elke kaart<br>
+        Symbool: verschillend op elke kaart<br>
+        Aantal: verschillend op elke kaart<br>
+        Arceerstijl: verschillend op elke kaart<br>
+        <ul>
+            <li>ruit gestreept blauw 2</li>
+            <li>kronkel gevuld rood 1</li>
+            <li>ovaal open groen 3</li>
+            <li>Kleur: hetzelfde op elke kaart (groen)</li>
+        </ul>
+        </ul>
+        <b>Voorbeeld set 3:</b><br>
+
+        Kleur: hetzelfde op elke kaart (groen)<br>
+        Symbool: hetzelfde op elke kaart (ruit)<br>
+        Aantal: verschillend op elke kaart<br>
+        Arceerstijl: verschillend op elke kaart<br>
+        <ul>
+            <li>ruit gevuld groen 2</li>
+            <li>ruit gestreept groen 3</li>
+            <li>ruit open groen 1</li>
+        </ul>
+        </p>
+        <button class="button" onclick="window.location.href='set.test';">
+            ga terug naar de menu
+        </button>
+    </div>
 </body>
 
 </html>
