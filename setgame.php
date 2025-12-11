0a03f97afd0c2fa6a8f8d3241bede17eb1d25fd4
stanvdzee <511111@student.firda.nl>
2025-12-11 13:58:46 +0100

    heel veel CSS

--- setgame.php
+++ setgame.php
@@ -1,3 +1,29 @@
 <button class="button" onclick="window.location.href='set.test';">
     ga terug naar de menu
 </button>
+<?php
+?><!DOCTYPE html>
+<html lang="nl">    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SET Spel</title>
+    <link rel="stylesheet" href="/style.css">
+    <script src="/script.js"></script>
+</head>
+<body>
+    <h1>Welkom bij het SET Spel</h1>
+</body>
+</html>
+<div class="container_field">
+    <div class="container1" id="leftie">
+        <button class="button" onclick="newGame()">nieuw Spel</button>
+        
+
+
+
+     </div>
+
+    <div class="container2" id="center"></div>
+
+</div>
